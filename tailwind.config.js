f1f0ffc4422316a145c7b10bcaf2aa3e2e06f2cd
Danielroxs <danroxsll@gmail.com>
2023-12-27 15:27:40 -0600

    Finalizacion de la pagina

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         "dark-grayish-blue": "hsl(227, 12%, 61%)",
         "very-dark-blue": "hsl(233, 12%, 13%)",
         "very-pale-red": "hsl(13, 100%, 96%)",
-        "very-light-gray": "hsl(0, 0%, 98%)",
+        "vary-light-gray": "hsl(0, 0%, 98%)",
       },
 
       backgroundImage: {
